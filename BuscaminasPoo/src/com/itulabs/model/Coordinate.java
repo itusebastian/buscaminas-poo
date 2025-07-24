71c7103edd6d1fa215cf9f66dd8043b1593e9b39
Steve Tutillo <stevetutillo8@gmail.com>
2025-07-24 08:52:04 -0500

    Implementar la clase Coordinate, incluidos el constructor

--- BuscaminasPoo/src/com/itulabs/model/Coordinate.java
+++ BuscaminasPoo/src/com/itulabs/model/Coordinate.java
@@ -1,5 +1,38 @@
 package com.itulabs.model;
 
-public class Coordinate {
+import java.util.Objects;
 
+public class Coordinate {
+    private final int x;
+    private final int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isWithinBounds(int size) {
+        return x >= 0 && y >= 0 && x < size && y < size;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Coordinate)) return false;
+        Coordinate other = (Coordinate) obj;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
