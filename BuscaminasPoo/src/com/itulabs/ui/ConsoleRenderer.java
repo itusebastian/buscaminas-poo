036d1a151b547bd647c6d0051b5a37a8d235f079
Diana Calderon <dianaeli1830@gmail.com>
2025-07-24 14:24:05 -0500

    Implementar la lógica de renderizado del tablero en la consola

--- BuscaminasPoo/src/com/itulabs/ui/ConsoleRenderer.java
+++ BuscaminasPoo/src/com/itulabs/ui/ConsoleRenderer.java
@@ -1,5 +1,56 @@
 package com.itulabs.ui;
 
+import com.itulabs.model.Board;
+import com.itulabs.model.Tile;
+
 public class ConsoleRenderer {
 
+    public void render(Board board) {
+        render(board, false);
+    }
+
+    public void render(Board board, boolean revealAll) {
+        int size = board.getSize();
+
+        // Encabezado de columnas (números 1 a size)
+        System.out.print("    ");
+        for (int col = 0; col < size; col++) {
+            System.out.printf("%2d ", col + 1);
+        }
+        System.out.println();
+
+        // Separador
+        System.out.print("    ");
+        for (int col = 0; col < size; col++) {
+            System.out.print("---");
+        }
+        System.out.println();
+
+        // Filas (letras A a ...)
+        for (int row = 0; row < size; row++) {
+            char rowLabel = (char) ('A' + row);
+            System.out.printf("%2c |", rowLabel);
+            for (int col = 0; col < size; col++) {
+                Tile tile = board.getTile(row, col);
+                if (tile.isFlagged()) {
+                    System.out.print(" F ");
+                } else if (revealAll) {
+                    if (tile.isMine()) {
+                        System.out.print(" * ");
+                    } else if (tile.isRevealed()) {
+                        System.out.print(tile.getDisplayValue());
+                    } else {
+                        System.out.print(" ? ");
+                    }
+                } else if (tile.isRevealed()) {
+                    System.out.print(tile.getDisplayValue());
+                } else {
+                    System.out.print(" ? ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
 }
