b3bfadef2e2dc6f4979682da079694a197652265
Diana Calderon <dianaeli1830@gmail.com>
2025-07-30 14:58:27 -0500

    Agregar pruebas unitarias para la clase MineTile y mejorar las pruebas de la clase NormalTile

--- BuscaminasPoo/src/test/com/itulabs/model/MineTileTest.java
+++ BuscaminasPoo/src/test/com/itulabs/model/MineTileTest.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class MineTileTest {
 
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIsMineAndDisplayValue() {
+		com.itulabs.model.MineTile tile = new com.itulabs.model.MineTile();
+		assertTrue(tile.isMine());
+		assertEquals(" ? ", tile.getDisplayValue());
+		tile.toggleFlag();
+		assertEquals(" F ", tile.getDisplayValue());
+		tile.toggleFlag();
+		tile.reveal();
+		assertEquals(" * ", tile.getDisplayValue());
 	}
 
 }
--- BuscaminasPoo/src/test/com/itulabs/model/NormalTileTest.java
+++ BuscaminasPoo/src/test/com/itulabs/model/NormalTileTest.java
@@ -6,9 +6,25 @@ import org.junit.jupiter.api.Test;
 
 class NormalTileTest {
 
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIsMineAndAdjacentMines() {
+		com.itulabs.model.NormalTile tile = new com.itulabs.model.NormalTile();
+		assertFalse(tile.isMine());
+		tile.setAdjacentMines(3);
+		assertEquals(3, tile.getAdjacentMines());
+	}
+
+	@Test
+	void testGetDisplayValue() {
+		com.itulabs.model.NormalTile tile = new com.itulabs.model.NormalTile();
+		assertEquals(" ? ", tile.getDisplayValue());
+		tile.toggleFlag();
+		assertEquals(" F ", tile.getDisplayValue());
+		tile.toggleFlag();
+		tile.reveal();
+		tile.setAdjacentMines(2);
+		assertEquals(" 2 ", tile.getDisplayValue());
 	}
 
 }
